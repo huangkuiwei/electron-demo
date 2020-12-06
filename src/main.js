f7b6cd6377395d3f7543d6d8424019606e2144a3
huangkuiwei <1017969390@qq.com>
2020-12-06 23:27:57 +0800

    update

--- src/main.js
+++ src/main.js
@@ -1,1 +1,38 @@
-console.log('hello world')
+const { app, BrowserWindow, ipcMain } = require('electron')
+const path = require('path')
+
+let mainWin
+
+async function createWindow () {
+  mainWin = new BrowserWindow({
+    width: 1600,
+    height: 900,
+    webPreferences: {
+      nodeIntegration: true,
+      nodeIntegrationInWorker: true,
+      webSecurity: true,
+      webviewTag: true
+    }
+  })
+
+  await mainWin.loadFile(path.resolve(__dirname, '../public/index.html'))
+
+  mainWin.openDevTools()
+}
+
+app.whenReady().then(async () => {
+  await createWindow()
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', async () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    await createWindow()
+  }
+})
+
